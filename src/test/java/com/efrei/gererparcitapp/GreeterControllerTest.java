de9ef402c6548597c2320ed4c0d047929a59072f
kyllian.bretagne <kyllian.bretagne@efrei.net>
2024-04-02 18:47:56 +0200

    First commit

---
+++ src/test/java/com/efrei/gererparcitapp/GreeterControllerTest.java
@@ -0,0 +1,25 @@
+package com.efrei.gererparcitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.containsString;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GreeterControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloEndpoint() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string(containsString("Hello")));
+    }
+}
